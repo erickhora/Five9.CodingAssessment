ecad2460d43b127fc0a6f5f0c7715bf9f7972dfa
Erick <erickn.hora@gmail.com>
2024-09-16 17:48:54 +0100

    Created Exception middleware

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Five9.CodingAssessment.Extensions.Exceptions;
+
+namespace Five9.CodingAssessment.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (LateEventException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
