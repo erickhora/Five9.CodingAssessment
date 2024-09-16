2ccc56537d77097a47e9204cb86be9174a546895
Erick <erickn.hora@gmail.com>
2024-09-16 16:55:51 +0100

    Created DbContext to handle database

---
+++ Data/CallCenterContext.cs
@@ -0,0 +1,18 @@
+﻿using Five9.CodingAssessment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Five9.CodingAssessment.Data
+{
+    public class CallCenterContext : DbContext
+    {
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<Queue> Queues { get; set; }
+
+        public CallCenterContext(DbContextOptions<CallCenterContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Agent>().HasMany(a => a.Queues);
+        }
+    }
+}
