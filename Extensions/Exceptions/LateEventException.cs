88bb2892223b6d413144a7069d9b3d7d9bf1b20e
Erick <erickn.hora@gmail.com>
2024-09-16 17:04:47 +0100

    Created LateEventException to handle exception on timestamp

---
+++ Extensions/Exceptions/LateEventException.cs
@@ -0,0 +1,7 @@
+﻿namespace Five9.CodingAssessment.Extensions.Exceptions
+{
+    public class LateEventException : Exception
+    {
+        public LateEventException(string message) : base(message) { }
+    }
+}
